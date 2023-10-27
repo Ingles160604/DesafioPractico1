9f0505197730ff61254a437f758ff2cec1b596fd
Ingles160604 <melvinguzman323@gmail.com>
2023-10-27 15:20:34 -0600

    se agrega el proyecto

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand text-uppercase" href="/">Rick and Morty</a>
+        </div>
+       </nav>
+  )
+}
+
+export default navbar
